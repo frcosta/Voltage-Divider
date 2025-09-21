4ce3d1c420edca030e22d9457a1b7dee1ab4e611
Fabiano Costa <frcosta@users.noreply.github.com>
2025-09-21 10:30:35 -0300

    Final version - Add option to include RL

--- resdiv.f90
+++ resdiv.f90
@@ -2,14 +2,15 @@
 ! GNU Fortran (tdm64-1) 10.3.0 on Windows 11
 ! Fabiano Costa Sep/20/2025
 ! compile: make all 
-! Add Load Calc
+! changes ---
+!    Sep/21/2025 Add option to include RL
 
 program res_divider
     use iso_c_binding
     implicit none
     
     integer(c_int) :: k
-    real :: Vin, Vout, r1, r2
+    real :: Vin, Vout, r1, r2, RL
 
     interface
         function getch() bind(C, name="getch")
@@ -18,11 +19,12 @@ program res_divider
         end function getch
     end interface
 
-    write(*,'(/, A, /)') '==== Voltage divider Calc ===='
+    write(*,'(/, A, /)') '====== Voltage divider Calc ======'
     write(*,'(A)')       '1) Vout? Select Vin, R1, R2'
     write(*,'(A)')       '2) R1?   Select Vin, Vout, R2'
-    write(*,'(A, /)')    '3) R2?   Select Vin, Vout, R1'
-    write(*,'(A)')       '=============================='
+    write(*,'(A)')       '3) R2?   Select Vin, Vout, R1'
+    write(*,'(A, /)')    '4) R1?   Select Vin, Vout, R2, RL'         
+    write(*,'(A)')       '=================================='
     write(*,'(A)', advance="no") 'Select your choice: '
     k = getch()
 
@@ -45,6 +47,13 @@ program res_divider
         read(*,*) Vin, Vout, r1
         r2 = (Vout * r1) / (Vin - Vout)
         write(*,'(/, A, F8.2, A)') 'R2 = ', r2, ' Ohms'
+    case (52)
+        write(*,'(2/,A)') 'R1 calculation based on Vin, Vout, R2 and RL'
+        write(*,'(A)', advance="no") 'Enter Vin, Vout, R2, RL separated by commas: '
+        read(*,*) Vin, Vout, r2, RL
+        r2 = (r2 * RL) / (r2 + RL)
+        r1 = ((r2 * Vin) / Vout) - r2
+        write(*,'(/, A, F8.2, A)') 'R1 = ', r1, ' Ohms'
     end select
 
 end program res_divider
